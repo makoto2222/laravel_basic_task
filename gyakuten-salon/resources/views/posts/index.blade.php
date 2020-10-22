b63710aefe34102297ac99098b416a4b474b6f76
EC2 Default User <ec2-user@ip-172-31-3-55.ap-northeast-1.compute.internal>
2020-10-22 22:40:27 +0000

    【Laravel 基礎課題4】 ご確認お願いいたします

--- gyakuten-salon/resources/views/posts/index.blade.php
+++ gyakuten-salon/resources/views/posts/index.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 
     <div class="container mt-4">
+        @foreach ($posts as $post)
         <div class="card mt-4">
             <div class="card-header mb-2">
-                Tile
+                {{ $post->title }}
             </div>
             <div class="card-body">
                 <p class="card-text">
-                    body
+                    {{ $post->body }}
                 </p>
             </div>
             <div class="card-footer">
@@ -17,7 +18,9 @@
                     投稿日時
                 </span>
             </div>
+        
         </div>
+        @endforeach
     </div> 
 
 
